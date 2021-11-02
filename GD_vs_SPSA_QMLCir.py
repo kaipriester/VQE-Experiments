ee81db60b8ff247de034961bb297fce55f471315
Kai Priester <kai@priester.net>
2021-11-02 15:21:21 -0400

    SPSA working but getting constant output for now

--- GD_vs_SPSA_QMLCir.py
+++ GD_vs_SPSA_QMLCir.py
@@ -4,6 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import time
 
+def sigma(first, last, const):
+    # first : is the first value of (n) (the index of summation)
+    # last : is the last value of (n)
+    # const : is the number that you want to sum its multiplication each (n) times with (n)
+    sum = 0
+    for i in range(first, last + 1):
+        sum += const * i
+    return sum
 
 def my_ansatz(p,w0,w1):
     qml.CNOT(wires=[w1,w0])
@@ -27,7 +35,7 @@ def my_circuit(params):
     my_ansatz(params[5],2,3)
         
 #@qml.qnode(device)
-def circuit(param, n=3, measurenum=0):
+def make_circuit(param, measurenum=0):
     # measurement circuit
     my_circuit(param)
     
@@ -39,22 +47,25 @@ def circuit(param, n=3, measurenum=0):
     elif measurenum==2: 
         measurestr='YYYY'
             
-        
+    n = 4    
     for qb in range(n):
         if (measurestr[qb] == 'X'):  # Hadamard transform between X and Z
             qml.Hadamard(wires=qb)
         if (measurestr[qb] == 'Y'):  # transform
             qml.S(wires=qb)
             qml.Hadamard(wires=qb)
-         
-    return qml.probs(wires=[0,1,2,3]) 
-        #return [qml.expval(qml.PauliZ(i)) for i in range(n_wires)]
 
-def GD(steps=10, n_wires=4, n_layers=6, stepsize=0.3, exact_E = -0.1026, device = qml.device("default.qubit", wires = 4, shots = 8000)):
+    return qml.probs(wires=[0,1,2,3]) 
+    # return [
+    #     qml.expval(qml.PauliX(0) @ qml.PauliY(1)),
+    #     qml.expval(qml.PauliX(0) @ qml.PauliZ(2))
+    # ]
+
+def GD(steps=10, n_wires=4, n_layers=6, stepsize=0.3, device = qml.device("default.qubit", wires = 4, shots = 8000)):
     #USE pennylane's GradientDescentOptimizer PACKAGE TO CALCULATE ENERGIES
 
     opt = qml.GradientDescentOptimizer(stepsize=stepsize)    
-    cost = qml.QNode(circuit, device)
+    cost = qml.QNode(make_circuit, device)
 
     params = qml.init.strong_ent_layers_normal(
         n_wires=n_wires, n_layers=n_layers
@@ -69,18 +80,21 @@ def GD(steps=10, n_wires=4, n_layers=6, stepsize=0.3, exact_E = -0.1026, device 
 
     return F   
 
-def SPSA(steps=10, n_wires=4, n_layers=6, c=0.3, a=1.5, exact_E = -0.1026, device = qml.device("default.qubit", wires = 4, shots = 8000)):
+def SPSA(steps=200, n_wires=4, n_layers=6, c=0.3, a=1.5, device = qml.device("default.qubit", wires = 4, shots = 8000)):
   #USE noisyopt's minimizeSPSA PACKAGE TO CALCULATE ENERGIES
-    qnode_spsa = qml.QNode(circuit, device)
+    qnode_spsa = qml.QNode(make_circuit, device)
 
     def cost_spsa(params):
-        return qnode_spsa(params.reshape(n_layers, n_wires, 3))
+        nda = qnode_spsa(params)
+        sum_a=0
+        A=0.1*np.ones(16) # weights, 16 for total 16 states, its value can be change​
+        for i,j in enumerate(nda):
+            sum_a=sum_a+A[i]*j
+        print(nda)
+        print(sum_a)    
+        return sum_a
 
-    flat_shape = n_layers * n_wires * 3
-    init_params = qml.init.strong_ent_layers_normal(
-        n_wires=n_wires, n_layers=n_layers
-    )
-    init_params_spsa = init_params.reshape(flat_shape)
+    init_params_spsa = [0,0,0,0,0,0]
 
     cost_store_spsa = [cost_spsa(init_params_spsa)]
     device_execs_spsa = [0]
@@ -107,7 +121,7 @@ def SPSA(steps=10, n_wires=4, n_layers=6, c=0.3, a=1.5, exact_E = -0.1026, devic
 
     return F
 
-def plot_result(F, exact_E):
+def plot_result(F, exact_E=0):
     Nitr=len(F)
     plt.figure()
     F = np.array(F)
@@ -124,15 +138,15 @@ if __name__ == '__main__':
     print("Running SPSA...")
     res1 = SPSA()
     time_stop_SPSA = time.time()
-    plot_result(res1, -1.136189454088)  
+    plot_result(res1)  
 
     time_start_GD = time.time()
     print("Running GD...")
     res2 = GD()
     time_stop_GD = time.time()
-    plot_result(res2, -1.136189454088)
+    plot_result(res2)
 
 
-    #print("SPSA time: " + str(time_stop_SPSA - time_start_SPSA))
+    print("SPSA time: " + str(time_stop_SPSA - time_start_SPSA))
     print("GD time: " + str(time_stop_GD - time_start_GD))
  
