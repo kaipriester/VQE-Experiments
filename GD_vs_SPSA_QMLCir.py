e4d0854c72e03c998c0eed40772f338777118b34
Kai Priester <kai@priester.net>
2021-11-05 16:13:06 -0400

    improved user interface and iteraction, same opt methods

--- GD_vs_SPSA_QMLCir.py
+++ GD_vs_SPSA_QMLCir.py
@@ -4,9 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import time
 
-from pennyl3 import make_circuit, sim_run
+from pennyl3 import sim_run
 
-def GD(steps=100, stepsize=0.3, init_params = np.pi/2*np.ones(6)):
+def GD(steps, init_params, stepsize):
     opt = qml.GradientDescentOptimizer(stepsize=stepsize)    
     cost = sim_run
 
@@ -20,7 +20,7 @@ def GD(steps=100, stepsize=0.3, init_params = np.pi/2*np.ones(6)):
 
     return F   
 
-def SPSA(steps=100, c=0.2, a=1.5, init_params = np.pi/2*np.ones(6)):
+def SPSA(steps, init_params, c, a):
     init_params_spsa = init_params
 
     F = []
@@ -42,7 +42,7 @@ def SPSA(steps=100, c=0.2, a=1.5, init_params = np.pi/2*np.ones(6)):
 
     return F
 
-def Adam(steps=100, stepsize=0.3, init_params = np.pi/2*np.ones(6)):
+def Adam(steps, init_params, stepsize):
     opt = qml.AdamOptimizer(stepsize=stepsize)    
     cost = sim_run
 
@@ -56,9 +56,9 @@ def Adam(steps=100, stepsize=0.3, init_params = np.pi/2*np.ones(6)):
 
     return F    
 
-def plot_result(F, exact_E=-0.1):
+def plot_result(F, opt_name, exact_E=-0.1):
     Nitr=len(F)
-    plt.figure()
+    fig = plt.figure()
     F = np.array(F)
     E = np.full((Nitr),exact_E)
     xv=np.arange(Nitr)+1
@@ -66,57 +66,109 @@ def plot_result(F, exact_E=-0.1):
     plt.plot(xv, E,'g--')
     plt.xlabel('iteration number')
     plt.ylabel('energy')
+    plt.savefig('results/' + opt_name + '_plot.png')
     plt.show()
+
+    return fig
 
 if __name__ == '__main__':
     print("In this program you can analyze the performance of different QML optimization methods on the quamtum circuit defined in pennyl3.py.")
     valid_input = False
 
     while not valid_input:
-        print("Menu: ")
-        print("     1. Basic gradient-descent optimizer")
+        print("     Menu: ")
+        print("     1. Basic gradient-descent optimizer (GD)")
         print("     2. Simultaneous Perturbation Stochastic Approximation (SPSA)")
-        print("     3. Gradient-descent optimizer with adaptive learning rate, first and second moment (AdamOptimizer)")
-        print("Input #: ", end='')
-        menu_input = input()
+        print("     3. Gradient-descent optimizer with adaptive learning rate, first and second moment (Adam)")
+        print("Input menu ID(s) of optimizers to run (examples: 1,3 or 2): ", end='')
+        menu_input = input()   
+        menu_input = list(menu_input.split(","))
 
-        if(menu_input == "1"):
-            valid_input = True
+        for i in menu_input:
+            if(i == '1'):
+                valid_input = True
+                print("GD hyperparameters (input d to use defaults OR any key to continue modifying): ", end='')
+                param_spec = input()
+                if(param_spec != "d"):
+                    print("Number of iterations (example: 300): ", end='')
+                    niter = int(input())
+                    print("Initial parameters (example: 1.57079633,1.57079633,1.57079633,1.57079633,1.57079633,1.57079633): ", end='')
+                    init_params = input()
+                    init_params = [float(item) for item in init_params.split(',')]
+                    print("Stepsize (example 0.2): ", end='')
+                    step_size = float(input())
+                else: 
+                    niter = 100
+                    init_params = np.pi/2*np.ones(6)
+                    step_size = 0.3    
+                
+                time_start = time.time()
+                print("Running GD-----------------------------------------------------------------------------------------------")
+                result = GD(niter, init_params, step_size)
+                time_stop = time.time()
+                print("GD plot saved to results directory")
+                print("GD time elapsed: " + str(time_stop - time_start))
+                print()
+                fig = plot_result(result, "GD")
 
-            time_start_GD = time.time()
-            print("Running GD-----------------------------------------------------------------------------------------------")
-            res2 = GD()
-            time_stop_GD = time.time()
-            plot_result(res2)
+            elif(i == '2'):
+                valid_input = True
+                print("SPSA hyperparameters (input d to use defaults OR any key to continue modifying): ", end='')
+                param_spec = input()
+                if(param_spec != "d"):
+                    print("Number of iterations (example: 300): ", end='')
+                    niter = int(input())
+                    print("Initial parameters (example: 1.57079633,1.57079633,1.57079633,1.57079633,1.57079633,1.57079633): ", end='')
+                    init_params = input()
+                    init_params = [float(item) for item in init_params.split(',')]
+                    print("c (example 0.2): ", end='')
+                    c = float(input())
+                    print("a (example 1.5): ", end='')
+                    a = float(input())
 
-            print("GD time " + str(time_stop_GD - time_start_GD))
+                else: 
+                    niter = 100
+                    init_params = np.pi/2*np.ones(6)
+                    c = 0.2
+                    a = 1.5  
 
-        elif(menu_input == "2"):
-            valid_input = True
-            time_start_SPSA = time.time()
-            print("Running SPSA---------------------------------------------------------------------------------------------")
-            res1 = SPSA()
-            time_stop_SPSA = time.time()
-            plot_result(res1)  
+                time_start = time.time()
+                print("Running SPSA---------------------------------------------------------------------------------------------")
+                result = SPSA(niter, init_params, c, a)
+                time_stop = time.time()
+                print("SPSA plot saved to results directory")
+                print("SPSA time elapsed: " + str(time_stop - time_start))
+                print()
+                fig = plot_result(result, "SPSA")
 
-            print("SPSA time: " + str(time_stop_SPSA - time_start_SPSA))
+            elif(i == '3'):
+                valid_input = True
+                print("Adam hyperparameters (input d to use defaults OR any key to continue modifying): ", end='')
+                param_spec = input()
+                if(param_spec != "d"):
+                    print("Number of iterations (example: 300): ", end='')
+                    niter = int(input())
+                    print("Initial parameters (example: 1.57079633,1.57079633,1.57079633,1.57079633,1.57079633,1.57079633): ", end='')
+                    init_params = input()
+                    init_params = [float(item) for item in init_params.split(',')]
+                    print("Stepsize(example 0.2): ", end='')
+                    step_size = float(input())
+                else: 
+                    niter = 100
+                    init_params = np.pi/2*np.ones(6)
+                    step_size = 0.3  
 
-        elif(menu_input == "3"):
-            valid_input = True
+                time_start = time.time()
+                print("Running Adam---------------------------------------------------------------------------------------------")
+                result = Adam(niter, init_params, step_size)
+                time_stop = time.time()
+                print("Adam plot saved to results directory")
+                print("Adam time elapsed: " + str(time_stop - time_start))
+                print()
+                fig = plot_result(result, "Adam")
 
-            time_start_Adam = time.time()
-            print("Running Adam---------------------------------------------------------------------------------------------")
-            res3 = Adam()
-            time_stop_Adam = time.time()
-            plot_result(res3)
 
-            print("Adam time: " + str(time_stop_Adam - time_start_Adam))
 
-        else:
-            valid_input = False
-            print("Invalid Input!")
-            print()    
-    
     
     
     
